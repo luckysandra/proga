6aa24e47ba6d7375007cb85086acd0a844340356
Студент НИУ ВШЭ <student@basm-401-std17.staff.corp.local>
2017-09-19 11:58:44 +0300

    проверка четности

---
+++ classwork/seminar1/parity_check.py
@@ -0,0 +1,17 @@
+a = int(input('Введите число: '))
+if a == 0:
+    print('Ноль')
+else:
+    if a % 2 == 0:
+        print('Четное')
+    else:
+        print('Нечетное')
+
+
+##a = int(input('Введите число: '))
+##if a == 0:
+##    print('Ноль')
+##elif a % 2 == 0:
+##    print('Четное')
+##else:
+##    print('Нечетное')
